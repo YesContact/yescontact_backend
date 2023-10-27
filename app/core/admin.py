4c296e7f6646cfc1865097b214abeca6384ea752
fardiab <feridabd37@gmail.com>
2023-10-27 12:47:36 +0400

    configure user admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'phone_number')
