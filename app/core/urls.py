90ac41f184f50b6a67db99041aed162d54a41119
fardiab <feridabd37@gmail.com>
2023-10-27 12:48:02 +0400

    fixing urls

---
+++ app/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from .views import *
+from .apis import *
+from .serializers import *
+
+urlpatterns = [
+    path('login', login, name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('register', UserRegistrationApiView.as_view(), name='register'),
+    path('', home, name='home'),
+]
