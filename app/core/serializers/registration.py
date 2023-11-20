ad3738aca12130f98d442a3a6c58328df2d55fcd
fardiab <feridabd37@gmail.com>
2023-11-20 23:32:07 +0400

    deleting files

--- app/core/serializers/registration.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from core.models import CustomUser
-
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ("full_name", "email", "phone_number", "password")
-        extra_kwargs = {"password": {"write_only": True}}
