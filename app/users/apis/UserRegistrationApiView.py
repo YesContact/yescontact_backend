8c35dfca83a86ae46efd9fa57dc6ee145c171e5f
fardiab <feridabd37@gmail.com>
2023-11-22 12:53:22 +0400

    fixing register apis

--- app/users/apis/UserRegistrationApiView.py
+++ app/users/apis/UserRegistrationApiView.py
@@ -7,7 +7,8 @@ from users.models import CustomUser
 from django.contrib.auth.forms import PasswordResetForm
 
 from users.serializers import UserRegistrationSerializer
-
+from django.conf import settings
+from app.utils import generate_otp, send_otp_email 
 
 class UserRegistrationApiView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -16,3 +17,12 @@ class UserRegistrationApiView(CreateAPIView):
         instance = serializer.save()
         instance.set_password(instance.password)
         instance.save()
+
+        # generate otp
+        otp = generate_otp()
+        instance.otp = otp
+        instance.is_verified = False
+        instance.save()
+
+        # send otp to user
+        send_otp_email(instance.email, otp)
