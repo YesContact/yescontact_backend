107ddada7b066538de3a02393229d37b4b82541b
fardiab <feridabd37@gmail.com>
2023-11-22 12:51:44 +0400

    otp configuration

--- app/users/urls/apis.py
+++ app/users/urls/apis.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.apis import UserRegistrationApiView, UserLoginAPIView, PasswordResetAPI
+from users.apis import UserRegistrationApiView, UserLoginAPIView, PasswordResetAPI, OTPVerificationAPI
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("password_reset/", PasswordResetAPI.as_view(), name="password_reset_api"),
+    path("otp_verification/", OTPVerificationAPI.as_view(), name="otp_verification_api"),
 ]
