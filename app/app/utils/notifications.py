776ba31f50f1a17fdfd4205ce539d5b8cce88507
fardiab <feridabd37@gmail.com>
2023-12-03 00:32:10 +0400

    adding notification func

---
+++ app/app/utils/notifications.py
@@ -0,0 +1,8 @@
+def send_notification(search_query):
+    # This is a placeholder for your notification logic
+    # Replace this with your actual notification mechanism
+    
+    # For example, sending an email notification
+    # This is just a simple illustration, replace it with your implementation
+    print(f"Notification sent for search query: {search_query}")
+    return True
