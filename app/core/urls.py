d49a445a307a1fe2e8fee0dd891edebd2b727269
fardiab <feridabd37@gmail.com>
2023-12-03 00:34:05 +0400

    configure urls

--- app/core/urls.py
+++ app/core/urls.py
@@ -8,6 +8,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("users", WhoSavedMNViewSet, basename="users")
+router.register('search-record', SearchRecordViewSet, basename='search-record')
 
 
 urlpatterns = [
@@ -15,6 +16,7 @@ urlpatterns = [
     path('take-numbers/', TakingNumbersViewSet.as_view({'get': 'list', 'post': 'create',}), name='take-numbers'),
     path('who-saved-mn/', WhoSavedMNViewSet.as_view({'get': 'list', 'post': 'create'}), name='who-saved-mn'),
     path('the-numbers-on-my-phone/', TheNumbersOnMyPhone.as_view({'get': 'list'}), name='the-numbers-on-my-phone'),
+    path('search-record/', SearchRecordViewSet.as_view({'post': 'create'}), name='search-record'),
 ]
 
 
