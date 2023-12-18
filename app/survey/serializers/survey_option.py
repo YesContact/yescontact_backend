d55a29b92f7c9296609b0f563a32e90dc64be1da
fardiab <feridabd37@gmail.com>
2023-12-19 03:16:06 +0400

    adding survey_option serializer

--- app/survey/serializers/survey_option.py
+++ app/survey/serializers/survey_option.py
@@ -3,6 +3,7 @@ from ..models import SurveyOption
 
 
 class SurveyOptionApiSerializer(serializers.ModelSerializer):
+    options = serializers.SerializerMethodField(source='get_options', read_only=True)
     class Meta:
         model = SurveyOption
         fields = '__all__'
