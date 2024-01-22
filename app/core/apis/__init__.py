17c67dcd53dc1330d6be31ee32393f54b6d4820d
fardiab <feridabd37@gmail.com>
2024-01-22 17:30:29 +0400

    adding tags api

--- app/core/apis/__init__.py
+++ app/core/apis/__init__.py
@@ -3,3 +3,4 @@ from .taking_numbers import TakingNumbersViewSet
 from .who_saved_number import WhoSavedMNViewSet
 from .the_number_on_my_phone import TheNumbersOnMyPhone
 from .search_record import SearchRecordViewSet
+from .tags import TagViewSet
